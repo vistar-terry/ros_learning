2e1b48be6a79ad0884fe47697eb75f4d9ec31fcb
holsey <1055311345@qq.com>
2020-12-12 20:36:25 +0800

    init

--- mbot_ws/build/learning_communication/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vistar/Desktop/mbot_ws/src/learning_communication
-# Build directory: /home/vistar/Desktop/mbot_ws/build/learning_communication
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
